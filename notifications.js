04319b4f5c017638fefe7885cb904db5709ff32b
Ken Hawkins <khawkins98@gmail.com>
2020-10-13 09:25:40 +0200

    Disable test

--- notifications.js
+++ notifications.js
@@ -1,15 +1,15 @@
 [
-  {
-    "title": "Test notification",
-    "body": "<p>This provides an alternate method of sending notifications during major outages where the normal methods of adding a notification might fail.</p>",
-    "field_notification_link": "",
-    "field_notification_position": "inline",
-    "field_notification_presentation": "persistent",
-    "field_notification_urls": "localhost/*",
-    "field_notification_button_text": "",
-    "cookieVersion": "1",
-    "cookieName": "fallbackNotification1",
-    "field_notification_auto_accept": "False",
-    "field_notification_cookie": "False"
-  }
+//   {
+//     "title": "Test notification",
+//     "body": "<p>This provides an alternate method of sending notifications during major outages where the normal methods of adding a notification might fail.</p>",
+//     "field_notification_link": "",
+//     "field_notification_position": "inline",
+//     "field_notification_presentation": "persistent",
+//     "field_notification_urls": "localhost/*",
+//     "field_notification_button_text": "",
+//     "cookieVersion": "1",
+//     "cookieName": "fallbackNotification1",
+//     "field_notification_auto_accept": "False",
+//     "field_notification_cookie": "False"
+//   }
 ]
